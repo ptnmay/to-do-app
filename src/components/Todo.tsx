6ff4c6390b3dddfd7bca673bc7f6693a6148bdae
Pittinan Saeyang <pittinansaeyang@gmail.com>
2026-08-01 18:53:22 +0700

    enter add

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
 // import TodoItems from "./TodoItems";
-import { RiCalendarTodoFill } from "react-icons/ri";
-
+import { RiCalendarTodoFill, RiDeleteBin6Line } from "react-icons/ri";
 import { FaCheckCircle, FaCircle } from "react-icons/fa";
-import { RiDeleteBin6Line } from "react-icons/ri";
 
 type Todo = {
   id: number;
@@ -39,6 +37,10 @@ const Todo = () => {
 			}))
 	};
 
+	const handleDel = (id: number) => {
+		setTasks((prev) => prev.filter((todo) => todo.id !== id));
+	}
+
 	return (
 	<div className="bg-white w-full max-w-md flex flex-col p-7 min-h-100 rounded-xl">
 	{/* title */}
@@ -48,21 +50,29 @@ const Todo = () => {
 	</div>
 
 	{/* input box */}
-	<div className="flex items-center mt-3 bg-teal-100 rounded-full h-10">
-		<input
-			className="bg-transparent outline-none flex-1 h-10 pl-6 pr-2"
-			type="text"
-			placeholder="Add your task."
-			value={task}
-			onChange={(e) => setTask(e.target.value)}
-		/>
-		<button
-			onClick={handleAdd}
-			className="bg-teal-800 hover:bg-teal-900 text-white w-20 h-full rounded-full cursor-pointer"
-		>
-			ADD
-		</button>
-		</div>
+	<form
+		className="flex items-center mt-3 bg-teal-100 rounded-full h-10"
+		onSubmit={(e) => {
+			e.preventDefault();
+			handleAdd();
+		}}
+	>
+	<input
+		maxLength={30}
+		className="bg-transparent outline-none flex-1 h-10 pl-6 pr-2"
+		type="text"
+		placeholder="Add your task."
+		value={task}
+		onChange={(e) => setTask(e.target.value)}
+	/>
+
+	<button
+		type="submit"
+		className="bg-teal-800 hover:bg-teal-900 text-white w-20 h-full rounded-full cursor-pointer"
+	>
+		ADD
+	</button>
+	</form>
 
 	{/* todo list */}
 	<div>
@@ -70,7 +80,7 @@ const Todo = () => {
 		// return <TodoItems key={todo.id} todo={todo} completedTodo={() => {}} />;
 		return (
 			<div className="flex item-center my-3 gap-2">
-			  <div className="flex flex-1 items-center cursor-pointer">
+				<div className="flex flex-1 items-center cursor-pointer">
 				{/* <FaCircle onClick={() => handleComplete(todo.id)} size={25} className="text-teal-800" /> */}
 				<div onClick={() => handleComplete(todo.id)}>
 					{todo.completed ? (
@@ -80,11 +90,12 @@ const Todo = () => {
 					)}
 				</div>
 				<p className="text-slate-700 ml-4 text-[17px]">{todo.text}</p>
-			  </div>
-			  <RiDeleteBin6Line
-				size={24}
-				className="text-teal-800 hover:text-teal-600 cursor-pointer"
-			  />
+				</div>
+				<RiDeleteBin6Line
+					onClick={() => handleDel(todo.id)}
+					size={24}
+					className="text-teal-800 hover:text-teal-600 cursor-pointer"
+				/>
 			</div>
 		  );
 		})}
